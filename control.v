964577718d9373f77dc7b219e490d0f2e185d6c8
lcy0x1 <arthurlcy0x1@gmail.com>
2021-11-16 16:14:40 -0800

    add comment for mult and control

--- control.v
+++ control.v
@@ -5,6 +5,31 @@
 Author: Arthur Wang
 Creation Date: Nov 14 
 Last Modified: Nov 16
+
+TODO: replaec wlr with w_ind == wsize[8:6]
+
+
+->  clk: clock
+->  enable: global enable, nothing shall be done if it is low
+->  reset: global reset, clears everything except memory
+->  operation: operation to be done
+->  in_data: input data, for reading matrix only
+
+Page Addresses:
+4 bits, highest bits tells which register file to use
+  use XRF for low, use WRF for high
+rest of it is for page number
+
+Operations: typically segregated by chunk of 4 bits
+chunk[0]: op code
+op code == 0: idle
+op code == 1:
+  chunk[1] = x page number (to read)
+  chunk[2] = w page number (to read)
+  chunk[3] = y page number (to write in bulk)
+  chunk[4] = configuration
+op code == 2:
+  chunk[1] = destination page number (to write in serial)
 */
 
 module controller(
@@ -14,66 +39,60 @@ module controller(
   input [31:0] operation,
   input [31:0] in_data
 );
-  
-  // operation [3:0] = mode
-  
-  // mode 1: perform calculation
-  // operation [7:4] = xaddr
-  // operation [11:8] = waddr
-  // operation [15:9] = yaddr
-  // operation [16] = use ReLU
-  // operation [17] = store B
-  
-  // mode 2: write data
-  // operation [7:4] = addr
-  
-  // mode
-  // 1: perform calculation
-  // 2: write w
-  // 3: write x
-
-  //addresses:
-  // 0000: X[0]
-  // 0001: X[1]
-  // 0010: X[2]
-  // 0011: X[3]
-  // 1000: W[0]
-  // 1001: W[1]
-  // 1010: W[2]
-  // 1011: W[3]
 
   wire [8:0] wsize = 9'b000011111; //TODO
   wire [8:0] xsize = 9'b001001111; //TODO
   
   // decode wire
-  wire [3:0] mode = operation[3:0];
+  wire [3:0] opcode = operation[3:0];
   wire [3:0] op_a = operation[7:4];
   wire [3:0] op_b = operation[11:8];
   wire [3:0] op_c = operation[15:12];
   
-  wire [3:0] wwe = mode == 2 ? op_a[3] == 1 ? 1 : 0 : 0;
-  wire [3:0] xwe = mode == 2 ? op_a[3] == 0 ? 1 : 0 : mode == 1 ? op_c[3] == 0 ? 2 : 0 : 0;
+  // write enable for register file W
+  wire [3:0] wwe = opcode == 2 ? op_a[3] == 1 ? 1 : 0 : 0;
+  // write enable for register file X
+  wire [3:0] xwe = opcode == 2 ? op_a[3] == 0 ? 1 : 0 : opcode == 1 ? op_c[3] == 0 ? 2 : 0 : 0;
   
-  wire [1:0] rws = mode == 1 ? op_b[3] == 0 ? op_b[1:0] : 0 : 0;
-  wire [1:0] wws = mode == 2 ? op_a[3] == 1 ? op_a[1:0] : 0 : 0;
-  wire [1:0] rxs = mode == 1 ? op_a[3] == 0 ? op_a[1:0] : 0 : 0;
-  wire [1:0] wxs = mode == 1 ? op_c[3] == 0 ? op_c[1:0] : 0 : mode == 2 ? op_a[3] == 0 ? op_a[1:0] : 0 : 0;
+  // read address for register file W
+  wire [1:0] rws = opcode == 1 ? op_b[3] == 0 ? op_b[1:0] : 0 : 0;
+  // write address for register file W
+  wire [1:0] wws = opcode == 2 ? op_a[3] == 1 ? op_a[1:0] : 0 : 0;
+  // read address for register file X
+  wire [1:0] rxs = opcode == 1 ? op_a[3] == 0 ? op_a[1:0] : 0 : 0;
+  // write address for register file X
+  wire [1:0] wxs = opcode == 1 ? op_c[3] == 0 ? op_c[1:0] : 0 : opcode == 2 ? op_a[3] == 0 ? op_a[1:0] : 0 : 0;
   
+
+
+  // enable flag for starting matrix multiplier (starting shifting data into multiplier)
+  // it should be on only for the duration that is required for memory to shift data
+  // so it is configured in such way that it is off BEFORE <opcode == 1> goes off
   wire en;
+  // delayed flag ofr <op_code == 1>, so that we can know the upper edge
+  reg old_en;
+  // cell index for W matrix, incremented every cycle
   reg [8:0] ind_w;
+  // line index for X matrix, incremented only after W matrix completes 1 cycle
   reg [2:0] ind_x;
   
+
+
+  // wires connecting memory and multiplier
   wire [31:0] w_in [7:0];
   wire [31:0] x_in [7:0];
   wire [7:0] clear_in;
   wire [31:0] y_out [7:0];
   wire [7:0] clear_out;
+
+  // output valid flag is 1 cycle delay of clear_out
   reg [7:0] y_valid;
+  // last read flag for W
   wire wlr;
   
+
+
   // empty wires as placeholder for unused ports
-  wire [31:0] t0 [7:0];
-  wire [7:0] t1;
   wire [31:0] t2 [7:0];
   wire [31:0] t3 [7:0];
   wire [31:0] zeros [7:0];
@@ -91,23 +110,25 @@ module controller(
   
   m8x8 mult(w_in, x_in, zeros, clear_in, enable, clear_out, clk, reset, t2, t3, y_out, clear_out);
 
-  reg old_en;
-  assign en = mode == 1 && !old_en || ind_w > 0 || ind_x > 0;
-  
+  // filter out the upper edge of <opcode == 1> and persist only when memory is still shifting data out.
+  assign en = opcode == 1 && !old_en || ind_w > 0 || ind_x > 0;
   
   always @(posedge clk) begin
-    if(reset) begin
+    if(reset) begin // reset behavior: clear all registers
         ind_w <= 0;
         ind_x <= 0;
       	y_valid <= 0;
       	old_en <= 0;
     end else if(enable) begin
       if(en) begin
+        // count the duration for memory to shift data out
         ind_w <= ind_w == wsize[8:0] ? 0 : ind_w + 1;
         ind_x <= ind_w == wsize[8:0] ? ind_x == xsize[8:6] ? 0 : ind_x + 1 : ind_x;
       end
+      // delay <clear_out> for writing memory in bulk
       y_valid <= clear_out;
-      old_en <= mode == 1;
+      // delay <op_code == 1>
+      old_en <= opcode == 1;
     end
   end
   
--- mult.v
+++ mult.v
@@ -10,6 +10,31 @@ TODO: allow transpose output
 module mac: multiply and accumulate module
 module m8x8: 8 by 8 matrix multiplication module
 
+->  clk: clock
+->  enable: global enable, nothing shall be done if it is low
+->  reset: reset everything
+
+->  w_in: vertical input
+<-  w_out: place to shift out vertical input, typically ignored
+->  x_in: horizontal input
+<-  x_out: plaec to shift out horizontal input, typically ignored
+->  y_in: typically zero, input of shift register
+<-  y_out: output of shift register
+->  clear_in: clear signal that clears accumulator
+<-  clear_out: place to shift out clear signal, typically used for <shift>
+->  shift: shift out data on a horizontal line, paired with y_out
+
+How it works:
+                 w_in
+                   |
+--------------+----|----------- shift
+              |    v
+              +-> [   ]
+x_in, clear_in -> [mac] -> x_out, clear_out
+         y_out <- [   ] <- y_in
+                   |
+                   v
+                w_out
 */
 
 module mac(
