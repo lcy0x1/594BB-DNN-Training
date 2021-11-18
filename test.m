929d6e9885825fe315caadbc056ac0a7eea8fa31
lcy0x1 <arthurlcy0x1@gmail.com>
2021-11-18 14:53:17 -0800

    add matlab file

---
+++ test.m
@@ -0,0 +1,14 @@
+W = zeros(64,64);
+X = zeros(64,64);
+seed = 1234567;
+for i=1:64
+    for j=1:64
+        W(i,j) = mod(seed,2);
+        seed = mod(seed * 17, 100007);
+        X(i,j) = mod(seed,2);
+        seed = mod(seed * 17, 100007);
+    end
+end
+%W(1:16,1:16)
+%X(1:16,1:16)
+ANS = X(1:16,1:16)*W(1:16,1:16)
